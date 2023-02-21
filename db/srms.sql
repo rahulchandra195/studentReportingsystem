81604b9ec3f6f179b3498fae894d6cc9a84bed77
Rahul kumar <42943345+rahulchandra195@users.noreply.github.com>
2023-02-21 14:53:17 +0530

    Update srms.sql

--- db/srms.sql
+++ db/srms.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 11, 2020 at 03:54 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
